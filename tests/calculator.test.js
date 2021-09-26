9bad618f3c70ebbf169034ea45f3e18912d2a68d
Johan Perera <johanpriyasanka@gmail.com>
2021-09-26 19:01:29 +0530

    calculator test cases

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -3,3 +3,15 @@ var calculator = require('../calculator');
 test("adds 1 and 2 and returns 3", () => {
     expect(calculator.add(1, 2)).toBe(3);
 });
+
+test("div 20 from 5 and returns 4", () => {
+    expect(calculator.division(20, 5).toBe(4));
+});
+
+test("Sub 3 from 13 and returns 10", () => {
+    expect(calculator.subtract(13, 3).toBe(10));
+});
+
+test("mul 3 by 4 and returns 12", () => {
+    expect(calculator.multiply(4, 3).toBe(12));
+});
